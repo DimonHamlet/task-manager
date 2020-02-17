8b03d98e1357fbbbb212c4f15e8b2429298dc44a
Dima <formyblogger69@gmail.com>
2020-02-17 23:29:32 +0200

    Adding delete functionality

--- src/index.js
+++ src/index.js
@@ -23,6 +23,61 @@ app.post('/users', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        
+        res.send(user)
+    }
+    catch (e) {
+        res.status(500).send({
+            error: e
+        })
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        
+        res.send(task)
+    }
+    catch (e) {
+        res.status(500).send({
+            error: e
+        })
+    }
+})
+
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdate = ['name', 'email', 'password'] 
+    const isValidUpdate = updates.every((update) => allowedUpdate.includes(update))
+    if (!isValidUpdate) {
+        return res.status(400).send({
+            error: "Invalid updates"
+        })
+    }
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, useValidators: true})
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }
+    catch (e) {
+        res.status(400).send({
+            error: e
+        })
+    }
+})
+
 app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
 
@@ -65,6 +120,29 @@ app.post('/tasks', async (req, res) => {
         res.status(400)
         res.send({
             error: error.message
+        })
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdate = ['description', 'completed'] 
+    const isValidUpdate = updates.every((update) => allowedUpdate.includes(update))
+    if (!isValidUpdate) {
+        return res.status(400).send({
+            error: "Invalid updates"
+        })
+    }
+    try {
+        const user = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, useValidators: true})
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }
+    catch (e) {
+        res.status(400).send({
+            error: e
         })
     }
 })
